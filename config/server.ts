6e77cf4a60d89ef290c2ac9155d942d969231901
Ravindra Nath Jha <ravindranathjha76@gmail.com>
2025-07-17 18:35:39 +0530

    Add backend URL configuration to admin and server settings

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('STRAPI_ADMIN_BACKEND_URL', 'https://strapi-production-4f32.up.railway.app'),
   app: {
     keys: env.array('APP_KEYS', [
       'defaultKey1',
