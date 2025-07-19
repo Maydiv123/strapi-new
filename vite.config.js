1ac95ba7b31736b0fbcfb0c423ab309ddb45db16
Ravindra Nath Jha <ravindranathjha76@gmail.com>
2025-07-20 02:05:13 +0530

    Add Vite configuration to specify allowed server hosts for development

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+export default {
+    server: {
+      allowedHosts: [
+        'healthcheck.railway.app',
+        'localhost',
+        '127.0.0.1',
+        'strapi-production-4f32.up.railway.app'
+      ]
+    }
+  }
